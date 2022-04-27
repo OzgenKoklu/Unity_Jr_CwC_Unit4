48436dab58a97362c58ef77db4fca9d3d9e54563
Ozgen Koklu <ozgen.koklu@gmail.com>
2022-04-27 18:17:00 +0300

    First Commit, optional challenges are missing, will update later

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private GameObject focalPoint;
+    private Rigidbody playerRb;
+    public bool hasPowerup = false;
+    private float powerUpStrength = 15;
+    public GameObject powerUpIndicator;
+    Vector3 powerUpOffset = new Vector3(0, -0.8f, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        focalPoint = GameObject.Find("Focal Point");
+        playerRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddForce(focalPoint.transform.forward * forwardInput * speed);
+
+        powerUpIndicator.transform.position = transform.position + powerUpOffset;
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Powerup"))
+        {
+            hasPowerup = true;
+            powerUpIndicator.SetActive(true);
+            Destroy(other.gameObject);
+            StartCoroutine(PowerupCountdownRoutine());
+        }
+    }
+
+    IEnumerator PowerupCountdownRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false; 
+        powerUpIndicator.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy") && hasPowerup)
+        {
+            Rigidbody enemyRigidbody = collision.gameObject.GetComponent<Rigidbody>();
+            Vector3 awayFromPlayer = collision.gameObject.transform.position - playerRb.transform.position;
+
+            enemyRigidbody.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
+            Debug.Log("Collided with: " + collision.gameObject.name + " with power up set to " + hasPowerup);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# Unity Learn JR pathway
+Unity Learn JR path is awesome, I would suggest it to anyone who wants to learn how to make games. It does also give important tips and tricks on what you should do in order to have a clean looking code. 
+
+# Short Gameplay clip - (coming after I complete bonus challenges)
+
+In Unity Learn, there are some game logic challenges that are completely optional, they go from easy to hard, and generally include things that are beyond the scope of the educational material\ 
+I find it fun to complete them, I will soon complete them when I have some spare time, and when I do, I will put a gameplay video here.\
+After completing the Jr path with all of it's challenges , I will also add all the scenes in one big package and release a WebGL version of the 10 mini games I've developed. Look forward to it.
+
+# Engine features, Game mechanics & Other on-screen actions using C# Scripting in this step by step tutorial
+Child object usage, fixing camera to a pivot point using nested objects \
+For Loops for spawning multiple enemies, also for different waves of enemies \
+Better structured code, putting recursive elements in methods, datas etc\
+Adding a power up, dealing with states using bool's. 
+
+# Notes
+This project was done for learning purposes.\
+I was away from unity for the last 2-3 months, to revitalize my skills and get faster for my future work, I followed the step by step tutorials offered by Unity. \
+I completed Unit 3 in around 5-10 hours since I already knew how to use unity.
+
+# -DISCLAIMER - Asset usage and sharing -
+The assets were used for learning purposes, I do not distribute or claim to own any rights on any assets included in the files.\
+Assets were downloaded through the Unity Learn itself. https://learn.unity.com/project/unit-4-gameplay-mechanics?uv=2020.3&pathwayId=5f7e17e1edbc2a5ec21a20af&missionId=5f7648a4edbc2a5578eb67df
